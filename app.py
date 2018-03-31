963a6bfa10d110b8b66ac687246c03c0739ae75b
Brett Vanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-03-31 11:43:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_session import Session
 import text_analysis
 
@@ -11,7 +11,12 @@ SESSION_TYPE = 'filesystem'
 app = Flask(__name__)
 app.config.from_object(__name__)
 # create Session instance
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 Session(app)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -20,9 +25,16 @@ def index():
 @app.route('/submission', methods = ['GET', 'POST'])
 def submission():
     if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
         file = request.files['file']
-        session['file_contents'] = file.read()
-        return redirect(url_for('analysis'))
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            session['file_contents'] = file.read()
+            return redirect(url_for('analysis'))
     return render_template('submission.html')
 
 @app.route('/analysis')
@@ -33,6 +45,9 @@ def analysis():
     neg_sent = analyzed_sent[0]
     return render_template('analysis.html', pos_sent=pos_sent, neg_sent=neg_sent)
 
+@app.errorhandler(413)
+def page_not_found(e):
+    return "Your error page for 413 status code", 413
 
 if __name__ == '__main__':
    app.run(debug=True)
