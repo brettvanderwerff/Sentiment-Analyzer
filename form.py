adf0c93935b5c2aa91e259bd321f9283c20b2b38
Brett.Vanderwerff <vande060@gmail.com>
2018-04-01 17:23:06 -0700

    added form file for flask wtforms class

---
+++ form.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired, Length
+from wtforms.widgets import TextArea
+
+class MyForm(FlaskForm):
+    name = TextAreaField('Your Text Below', validators=[DataRequired(), Length(max=1500000)], widget=TextArea())
